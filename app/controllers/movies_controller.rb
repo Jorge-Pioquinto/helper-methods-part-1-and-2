e7ce07a9d7781b0a149c825434328621dd226f5b
Jorge Pioquinto <129575128+Jorge-Pioquinto@users.noreply.github.com>
2023-10-19 14:04:00 +0000

    finished using named route helpers instead of raw URLs

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render({ :template => "movies/index" })
+        render template:"movies/index"
       end
     end
   end
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to(movies_url, { :notice => "Movie created successfully." })
     else
       render template: "movies/new"
     end
@@ -63,10 +63,11 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movie_url(the_movie), notice: "Movie updated successfully."
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movie_url(the_movie), alert: "Movie failed to update successfully."
     end
+    
   end
 
   def destroy
@@ -75,6 +76,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
